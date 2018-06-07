0ae7a5a605becbedeb5dcfe25ec6cf40e8542b81
Christopher Jones <cjones999983@gmail.com>
2018-06-07 01:21:46 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -22,7 +22,7 @@ end
 
 def select_name_and_series_subgenres_of_authors
   "SELECT authors.name, subgenres.name
-  FROM authors
+  FROM authors, subgenres
   INNER JOIN series
   ON series.author_id = authors.id
   INNER JOIN subgenres
