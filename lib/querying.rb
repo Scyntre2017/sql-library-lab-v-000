c61087dcfe70549efe12857e1b3d49d0126076a0
Christopher Jones <cjones999983@gmail.com>
2018-06-07 01:38:45 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -46,5 +46,5 @@ def select_character_names_and_number_of_books_they_are_in
   INNER JOIN characters
   ON character_books.character_id = characters.id
   GROUP BY characters.name
-  ORDER BY COUNT(*) DESC"
+  ORDER BY COUNT(*) DESC;"
 end
