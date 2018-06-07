346128580bed7c23296ca327d4b51dafdafd822e
Christopher Jones <cjones999983@gmail.com>
2018-06-07 01:12:58 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -24,7 +24,9 @@ def select_name_and_series_subgenres_of_authors
   "SELECT authors.name, sub-genres.name
   FROM authors, sub-genres
   INNER JOIN series
-  ON authors.id = series.author_id"
+  ON authors.id = series.author_id
+  INNER JOIN series
+  ON subgenres.id = series.subgenre_id;"
 end
 
 def select_series_title_with_most_human_characters
